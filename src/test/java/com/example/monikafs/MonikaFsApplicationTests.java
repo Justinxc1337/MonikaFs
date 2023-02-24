9c0c7c1ec59c0a59343e4b0980f0086c8f0f73f0
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2023-02-24 16:33:48 +0100

    I am sad

---
+++ src/test/java/com/example/monikafs/MonikaFsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monikafs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonikaFsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
